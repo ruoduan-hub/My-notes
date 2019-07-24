dbcb10651bde012f4ebc4eea8ad2be0503c10d77
chad97 <chad97@126.com>
2019-07-24 10:08:37 +0800

    二分法

---
+++ 数据结构算法/二分查找.py
@@ -0,0 +1,20 @@
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+arr = [1,4,2,5,7]
+print binary_search(arr, 5)
+
+            
